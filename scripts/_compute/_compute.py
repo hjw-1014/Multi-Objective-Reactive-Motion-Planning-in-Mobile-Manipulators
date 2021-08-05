Fix vector field using rrt tree, but need to clear the code and change to batch computation. | 08.05

scripts/_compute/_compute.py
@@ -38,7 +38,7 @@ def compute_cur_dist_graph_points_batch(cur_position: list, graph_rrt_son: np.ar
     return np.around(current_distances_from_path_points_son, 2).tolist(), np.around(current_distances_from_path_points_father, 2).tolist()
 
 def compute_cur_dist_graph_points(cur_position, graph_rrt_son: np.array, graph_rrt_father: np.array) -> list:
-    '''
+    ''' # TODO: added on 08.05, good but we need batch computation, so this function just for test
         return the distances between current position and all the points of the path
     '''
 
@@ -146,14 +146,12 @@ def choose_min_dist_point(tiago_env, num_path_points, current_distances_from_pat
     else:
         return end_point_threshold, num_path_points - 1
 
-def choose_min_dist_point_gragh(tiago_env,
+def choose_min_dist_point_graph_batch(tiago_env,
                                 cur_dist_son: list,
                                 cur_dist_father: list,
-                                graph_rrt: np.array,
                                 graph_rrt_son: np.array,
-                                graph_rrt_father: np.array,
-                                rrt_path: np.array) -> list:
-    # TODO: Need to update if add a path gragh | add on 08.03
+                                graph_rrt_father: np.array) -> list:
+    # TODO: Need to update if add a path gragh | added on 08.03
     '''
         Firstly, check if this point is already close enough to the end point.
         if not:
@@ -180,13 +178,56 @@ def choose_min_dist_point_gragh(tiago_env,
             # TODO: Thinking how to move to the next point (Father node) which is not inside the cascade_threshold on 08.04 !!!!!!!!!!!!!
             father_node_dist = cur_dist_father[son_dist_index]
             father_node = graph_rrt_father[son_dist_index].tolist()
-            if math.hypot(father_node[0] - end_point[0], father_node[1] - end_point[1]) < delta:
-                return end_point
-            elif father_node_dist[0] <= cascade_threshold and math.hypot(father_node[0]-end_point[0], father_node[1] - end_point[1]) >= delta:
+            # if math.hypot(father_node[0] - end_point[0], father_node[1] - end_point[1]) < delta:
+            #     return end_point
+            # elif father_node_dist[0] <= cascade_threshold and math.hypot(father_node[0]-end_point[0], father_node[1] - end_point[1]) >= delta:
+            #     son_dist_index = cur_dist_son.index(father_node_dist)
+            #     son_dist = cur_dist_father[son_dist_index]
+            # elif father_node_dist[0] > cascade_threshold and math.hypot(father_node[0]-end_point[0], father_node[1] - end_point[1]) >= delta:
+            #     return father_node
+            # TODO: Change here 08.05
+            if father_node_dist[0] >= cascade_threshold:
+                return father_node
+            elif father_node_dist[0] < cascade_threshold:
+                if math.hypot(father_node[0]-end_point[0], father_node[1]-end_point[1]) < delta \
+                        or father_node_dist[0] <= end_point_threshold:
+                    return father_node
                 son_dist_index = cur_dist_son.index(father_node_dist)
                 son_dist = cur_dist_father[son_dist_index]
-            elif father_node_dist[0] > cascade_threshold and math.hypot(father_node[0]-end_point[0], father_node[1] - end_point[1]) >= delta:
+
+        return graph_rrt_son[son_dist_index].tolist()
+
+def choose_min_dist_point_graph_batch_viz(cur_position: list,  # TODO: -> added on 08.05
+                                cur_dist_son: list,
+                                cur_dist_father: list,
+                                graph_rrt_son: np.array,
+                                graph_rrt_father: np.array) -> (float, int):
+
+    cur_end_dist = math.hypot(cur_position[0]-end_point[0], cur_position[1]-end_point[1])
+
+    if end_point_threshold <= cur_end_dist <= delta:
+        return end_point
+
+    elif cur_end_dist < end_point_threshold:
+        return end_point
+
+    elif cur_end_dist > delta:
+        son_dist = min(cur_dist_son)
+        son_dist_index = cur_dist_son.index(son_dist)
+        while son_dist[0] < cascade_threshold:
+            # TODO: Thinking how to move to the next point (Father node) which is not inside the cascade_threshold on 08.04 !!!!!!!!!!!!!
+            # TODO: Make a change on 08.05, works
+            father_node_dist = cur_dist_father[son_dist_index]
+            father_node = graph_rrt_father[son_dist_index].tolist()
+            father_end_dist = math.hypot(father_node[0] - end_point[0], father_node[1] - end_point[1])
+            if father_node_dist[0] >= cascade_threshold:
                 return father_node
+            elif father_node_dist[0] < cascade_threshold:
+                if father_end_dist <= delta:
+                    return father_node
+                print('father_end_dist: ', father_end_dist)
+                son_dist_index = cur_dist_son.index(father_node_dist)
+                son_dist = cur_dist_father[son_dist_index]
 
         return graph_rrt_son[son_dist_index].tolist()
 
@@ -211,8 +252,8 @@ def choose_min_dist_point_graph_viz(cur_position: list,  # TODO: -> add on 08.04
             print('2')
             # TODO: Thinking how to move to the next point (Father node) which is not inside the cascade_threshold on 08.04 !!!!!!!!!!!!!
             father_node_dist = cur_dist_father[son_dist_index]
-            print("son_dist: ", son_dist)
-            print('father_node_dist: ', father_node_dist)
+            # print("son_dist: ", son_dist)
+            # print('father_node_dist: ', father_node_dist)
             father_node = graph_rrt_father[son_dist_index].tolist()
             father_end_dist = math.hypot(father_node[0] - end_point[0], father_node[1] - end_point[1])
             if father_end_dist < delta:
@@ -223,12 +264,6 @@ def choose_min_dist_point_graph_viz(cur_position: list,  # TODO: -> add on 08.04
                 son_dist_index = cur_dist_son.index(father_node_dist)
                 son_dist = cur_dist_father[son_dist_index]
                 father_node_idx = cur_dist_father.index(father_node_dist)
-
-                print("father_node_dist: ", father_node_dist)
-                print('father_end_dist: ', father_end_dist)
-                print("father_node_idx :", father_node_idx)
-                print("son_dist_index: ", son_dist_index)
-                print("son_dist:", son_dist)
             elif father_node_dist >= cascade_threshold:
                 print("5")
                 return father_node

scripts/_controller/_controller.py
@@ -25,7 +25,7 @@ def cascade_control(tiago_env, num_path_points, current_state, xy_traj) -> list:
 
     return dx
 
-def cascade_control_rrt_tree(tiago_env, current_state, graph_rrt, graph_rrt_son, graph_rrt_father, rrt_path) -> list:
+def cascade_control_rrt_tree(tiago_env, current_state, graph_rrt_son, graph_rrt_father) -> list:
     '''
         current_state:
             current_state[0] -> current position [x, y]
@@ -37,10 +37,10 @@ def cascade_control_rrt_tree(tiago_env, current_state, graph_rrt, graph_rrt_son,
     dx = [0] * len(current_state)  # y and x
 
     cur_dist_son, cur_dist_father = compute_cur_dist_graph_points_batch(current_position, graph_rrt_son, graph_rrt_father)
-    closest_point = choose_min_dist_point_gragh(tiago_env,
+    closest_point = choose_min_dist_point_graph_batch(tiago_env,
                                                    cur_dist_son,
                                                    cur_dist_father,
-                                                   graph_rrt, graph_rrt_son, graph_rrt_father, rrt_path)
+                                                   graph_rrt_son, graph_rrt_father)
 
     for i in range(len(current_position)):  # TODO, change on 07.24
         dx[i] = -k_d * (current_position[i] - closest_point[i])
@@ -86,7 +86,7 @@ def cascade_control_all_points(xy_traj, min_x=-1.5, min_y=-1.5, max_x=1.5, max_y
 
     return velocity_map_arr
 
-def cascade_control_all_nodes_rrtTree(
+def cascade_control_all_nodes_rrtTree_viz(
                                       graph_rrt_son,
                                       graph_rrt_father,
                                       min_x=-1.5, min_y=-1.5, max_x=1.5, max_y=1.5,
@@ -108,12 +108,12 @@ def cascade_control_all_nodes_rrtTree(
     for i in range(len(grid_map)):  # Transverse all the points in the map
         print("grid_map: ", i)
         cur_pos = grid_map[i]  # current position
+        print('cur_pos: ', cur_pos)
         dx = [0] * len(cur_pos)  # -> [vel_x, vel_y]
         cur_pos_in_list = cur_pos.tolist()
         cur_dist_son, cur_dist_father = compute_cur_dist_graph_points_batch(cur_pos_in_list, graph_rrt_son, graph_rrt_father)
-        cur_dist_son, cur_dist_father = compute_cur_dist_graph_points(cur_pos_in_list, graph_rrt_son, graph_rrt_father)
-        print('1')
-        closest_point = choose_min_dist_point_graph_viz(
+        #cur_dist_son, cur_dist_father = compute_cur_dist_graph_points(cur_pos_in_list, graph_rrt_son, graph_rrt_father)
+        closest_point = choose_min_dist_point_graph_batch_viz(
                                 cur_pos_in_list,
                                 cur_dist_son,
                                 cur_dist_father,
@@ -127,7 +127,6 @@ def cascade_control_all_nodes_rrtTree(
             dx[j] = attractive_force[j] + repulsive_force[j]  # x and y velocity
         dx_arr = np.asarray(dx)  # TODO: Add repulsive potential field
         velocity_map.append(dx_arr)
-        print('6')
     velocity_map_arr = np.asarray(velocity_map)
 
     return velocity_map_arr

scripts/_scripts_run/rrt_tree_baseline_demo.py
@@ -1,14 +1,9 @@
-import numpy as np
 import pybullet as p
-import time
 from utils import *
 from _pybullet import start_bullet_env
 from _controller import *
 from _compute import *
 from _plot import *
-# sys.path.append("/home/ias/Documents/Jiawei/comp_energy_policy-Tiago/comp_energy_policy-main/scripts/PathPlanning/Sampling_based_Planning/rrt_2D/")
-# sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + \
-#                 'PathPlanning/Sampling_based_Planning/rrt_2d')
 
 class Node:
     def __init__(self, n: list):
@@ -18,7 +13,8 @@ class Node:
 
 
 if __name__ == "__main__":
-    # TODO: Read json trajectory
+
+    # TODO: Read json trajectory from moveit and rrt tree
     traj, num_path_points = open_json('qtrjs.json')
     xy_traj = get_x_y_traj(traj)
 
@@ -27,7 +23,6 @@ if __name__ == "__main__":
     graph_rrt = load_rrt_nodes_list("graph_rrt.npy") / 100  # numpy.ndarray(np.array)
     graph_rrt = np.around(graph_rrt, 3)
     #rrt_vertex = load_rrt_vertex("vertex_rrt.npy")
-
     graph_rrt_son, graph_rrt_father = split_son_father(graph_rrt)
 
     # TODO: tiago environment
@@ -53,10 +48,9 @@ if __name__ == "__main__":
         # TODO: Check if arrive the target point
         # Calculate the distance between end point and current point
         cur_dist = compute_euclidean_distance(end_point, cur_position)
-        ic(cur_position)
-        ic(cur_dist)
         if cur_dist < end_point_threshold:
             ic(cur_position)
+            ic(cur_dist)
             print("######### End point arrived!!! #########")
             print("--- %s seconds!!! ---" % (time.time() - start_time))
             plot_cascade_control_traj(cascade_control_path, rrt_path, len(rrt_path))  # TODO: Plot generated path
@@ -64,15 +58,13 @@ if __name__ == "__main__":
             break
 
         # Based on the current state, calculate the velocity command
-
-        dx = cascade_control_rrt_tree(tiago_env, current_state,
-                                      graph_rrt, graph_rrt_son, graph_rrt_father, rrt_path)
-        ic(dx)
+        dx = cascade_control_rrt_tree(tiago_env, current_state, graph_rrt_son, graph_rrt_father)
 
         cascade_control_path.append(cur_position)  # TODO: Record one path
 
         for i in range(len(next_position)):
             next_position[i] = cur_position[i] + delta * dx[i]
 
+        # Start pybullet engine
         tiago_env.start_baseline_resetJointState(next_position)
         p.stepSimulation()