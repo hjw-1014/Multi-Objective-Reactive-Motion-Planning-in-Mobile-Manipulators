Fix vector field using rrt tree, but need to clear the code and change to batch computation. | 08.05

scripts/PathPlanning/Sampling_based_Planning/rrt_2D/my_utils.py
@@ -144,7 +144,7 @@ class Utils:
         nodes_list.append(root)
 
     @staticmethod
-    def vertex_2_xy_list(vertex):  # TODO: add on 08.03
+    def vertex_2_xy_list(vertex):  # TODO: added on 08.03
 
         node_arr = np.zeros((2, 2))
         nodes_list = []
@@ -152,6 +152,8 @@ class Utils:
 
         for i in range(num_nodes):
             if not vertex[i].parent:
+                node_arr[0][0] = 120
+                node_arr[0][1] = 100
                 node_arr[1][0] = 120
                 node_arr[1][1] = 100
             else:
@@ -160,6 +162,7 @@ class Utils:
                 node_arr[1][0] = vertex[i].parent.x  # father node x
                 node_arr[1][1] = vertex[i].parent.y  # father node y
             nodes_list.append(deepcopy(node_arr))
+            node_arr = np.zeros((2, 2))
         ic(num_nodes)
         ic(len(nodes_list))
         return nodes_list

scripts/_compute/_compute.py
@@ -37,6 +37,22 @@ def compute_cur_dist_graph_points_batch(cur_position: list, graph_rrt_son: np.ar
 
     return np.around(current_distances_from_path_points_son, 2).tolist(), np.around(current_distances_from_path_points_father, 2).tolist()
 
+def compute_cur_dist_graph_points(cur_position, graph_rrt_son: np.array, graph_rrt_father: np.array) -> list:
+    '''
+        return the distances between current position and all the points of the path
+    '''
+
+    current_distances_from_path_points_son = []
+    current_distances_from_path_points_father = []
+    for i in range(len(graph_rrt_son)):
+        dist_son = compute_euclidean_distance(cur_position, graph_rrt_son[i])
+        current_distances_from_path_points_son.append(dist_son)
+        dist_father = compute_euclidean_distance(cur_position, graph_rrt_father[i])
+        current_distances_from_path_points_father.append(dist_father)
+
+    return current_distances_from_path_points_son, current_distances_from_path_points_father
+
+
 def compute_current_distance_from_path_points(cur_position, xy_traj) -> list:
     '''
         return the distances between current position and all the points of the path
@@ -124,10 +140,6 @@ def choose_min_dist_point(tiago_env, num_path_points, current_distances_from_pat
                 return min_dist, min_dist_index - 1
 
             min_dist = current_distances_from_path_points[min_dist_index]
-        # ic(min_dist)
-
-        # idx += 1
-        # min_dist_index = current_distances_from_path_points.index(min_dist)
 
         return min_dist, min_dist_index
 
@@ -135,7 +147,6 @@ def choose_min_dist_point(tiago_env, num_path_points, current_distances_from_pat
         return end_point_threshold, num_path_points - 1
 
 def choose_min_dist_point_gragh(tiago_env,
-                                rrt_vertex,
                                 cur_dist_son: list,
                                 cur_dist_father: list,
                                 graph_rrt: np.array,
@@ -196,20 +207,30 @@ def choose_min_dist_point_graph_viz(cur_position: list,  # TODO: -> add on 08.04
     elif cur_end_dist > delta:
         son_dist = min(cur_dist_son)
         son_dist_index = cur_dist_son.index(son_dist)
-
-        while son_dist[0] < cascade_threshold:
+        while son_dist < cascade_threshold:
             print('2')
             # TODO: Thinking how to move to the next point (Father node) which is not inside the cascade_threshold on 08.04 !!!!!!!!!!!!!
             father_node_dist = cur_dist_father[son_dist_index]
+            print("son_dist: ", son_dist)
+            print('father_node_dist: ', father_node_dist)
             father_node = graph_rrt_father[son_dist_index].tolist()
             father_end_dist = math.hypot(father_node[0] - end_point[0], father_node[1] - end_point[1])
             if father_end_dist < delta:
-                return end_point  # TODO: THINKING!!! 08.04
-            elif father_node_dist[0] <= cascade_threshold and father_end_dist >= delta:
                 print('3')
+                return end_point  # TODO: THINKING!!! 08.04
+            elif father_node_dist < cascade_threshold:
+                print('4')
                 son_dist_index = cur_dist_son.index(father_node_dist)
                 son_dist = cur_dist_father[son_dist_index]
-            elif father_node_dist[0] > cascade_threshold and father_end_dist >= delta:
+                father_node_idx = cur_dist_father.index(father_node_dist)
+
+                print("father_node_dist: ", father_node_dist)
+                print('father_end_dist: ', father_end_dist)
+                print("father_node_idx :", father_node_idx)
+                print("son_dist_index: ", son_dist_index)
+                print("son_dist:", son_dist)
+            elif father_node_dist >= cascade_threshold:
+                print("5")
                 return father_node
 
         return graph_rrt_son[son_dist_index].tolist()

scripts/_controller/_controller.py
@@ -25,7 +25,7 @@ def cascade_control(tiago_env, num_path_points, current_state, xy_traj) -> list:
 
     return dx
 
-def cascade_control_rrt_tree(tiago_env, current_state, rrt_vertex, graph_rrt, graph_rrt_son, graph_rrt_father, rrt_path) -> list:
+def cascade_control_rrt_tree(tiago_env, current_state, graph_rrt, graph_rrt_son, graph_rrt_father, rrt_path) -> list:
     '''
         current_state:
             current_state[0] -> current position [x, y]
@@ -38,7 +38,6 @@ def cascade_control_rrt_tree(tiago_env, current_state, rrt_vertex, graph_rrt, gr
 
     cur_dist_son, cur_dist_father = compute_cur_dist_graph_points_batch(current_position, graph_rrt_son, graph_rrt_father)
     closest_point = choose_min_dist_point_gragh(tiago_env,
-                                                   rrt_vertex,
                                                    cur_dist_son,
                                                    cur_dist_father,
                                                    graph_rrt, graph_rrt_son, graph_rrt_father, rrt_path)
@@ -88,10 +87,8 @@ def cascade_control_all_points(xy_traj, min_x=-1.5, min_y=-1.5, max_x=1.5, max_y
     return velocity_map_arr
 
 def cascade_control_all_nodes_rrtTree(
-                                      graph_rrt,
                                       graph_rrt_son,
                                       graph_rrt_father,
-                                      rrt_path,
                                       min_x=-1.5, min_y=-1.5, max_x=1.5, max_y=1.5,
                                       n_sample=301) -> np.array:  # TODO: Add repulsive potential field
     ''' # TODO: Add on 07.24
@@ -114,23 +111,23 @@ def cascade_control_all_nodes_rrtTree(
         dx = [0] * len(cur_pos)  # -> [vel_x, vel_y]
         cur_pos_in_list = cur_pos.tolist()
         cur_dist_son, cur_dist_father = compute_cur_dist_graph_points_batch(cur_pos_in_list, graph_rrt_son, graph_rrt_father)
-        print("cur_dist_son")
+        cur_dist_son, cur_dist_father = compute_cur_dist_graph_points(cur_pos_in_list, graph_rrt_son, graph_rrt_father)
+        print('1')
         closest_point = choose_min_dist_point_graph_viz(
                                 cur_pos_in_list,
                                 cur_dist_son,
                                 cur_dist_father,
                                 graph_rrt_son,
                                 graph_rrt_father)  # Get the closest point and it's index
-        print('1')
         # Potential field forces
         attractive_force = compute_attractive_potential_force(current_position=cur_pos, closest_point=closest_point)
-        repulive_force = compute_repulsive_potential_force(cur_pos)
+        repulsive_force = compute_repulsive_potential_force(cur_pos)
 
         for j in range(len(cur_pos)):  # TODO, change on 07.24 # Based on the distance, get the attractive potential
-            dx[j] = attractive_force[j] + repulive_force[j]  # x and y velocity
+            dx[j] = attractive_force[j] + repulsive_force[j]  # x and y velocity
         dx_arr = np.asarray(dx)  # TODO: Add repulsive potential field
         velocity_map.append(dx_arr)
-
+        print('6')
     velocity_map_arr = np.asarray(velocity_map)
 
     return velocity_map_arr

scripts/_scripts_run/rrt_tree_baseline_demo.py
@@ -23,10 +23,10 @@ if __name__ == "__main__":
     xy_traj = get_x_y_traj(traj)
 
     rrt_path = load_rrt_path('path_rrt.npy') / 100  # numpy.ndarray
-    rrt_path = np.around(rrt_path, 4)
+    rrt_path = np.around(rrt_path, 3)
     graph_rrt = load_rrt_nodes_list("graph_rrt.npy") / 100  # numpy.ndarray(np.array)
-    graph_rrt = np.around(graph_rrt, 4)
-    rrt_vertex = load_rrt_vertex("vertex_rrt.npy")
+    graph_rrt = np.around(graph_rrt, 3)
+    #rrt_vertex = load_rrt_vertex("vertex_rrt.npy")
 
     graph_rrt_son, graph_rrt_father = split_son_father(graph_rrt)
 
@@ -65,7 +65,7 @@ if __name__ == "__main__":
 
         # Based on the current state, calculate the velocity command
 
-        dx = cascade_control_rrt_tree(tiago_env, current_state, rrt_vertex,
+        dx = cascade_control_rrt_tree(tiago_env, current_state,
                                       graph_rrt, graph_rrt_son, graph_rrt_father, rrt_path)
         ic(dx)
 

scripts/_scripts_run/vector_field_rrt_tree_viz.py
@@ -18,6 +18,7 @@ if __name__ == "__main__":
 	rrt_path = np.around(rrt_path, 2)
 	graph_rrt = load_rrt_nodes_list("graph_rrt.npy") / 100  # numpy.ndarray(np.array)
 	graph_rrt = np.around(graph_rrt, 2)
+	rrt_vertex = load_rrt_vertex("vertex_rrt.npy")
 
 	graph_rrt_son, graph_rrt_father = split_son_father(graph_rrt)
 
@@ -33,7 +34,7 @@ if __name__ == "__main__":
 	XY = np.meshgrid(x, y)  # TODO: So each step is 0.01,  XY is a list
 
 	# Get the velocity map with dimension np.array((301x301, 2))
-	uv = cascade_control_all_nodes_rrtTree(graph_rrt, graph_rrt_son,  graph_rrt_father, rrt_path, min_x=-1.5, min_y=-1.5, max_x=1.5, max_y=1.5, n_sample=301)  # TODO: change to rrt graph 08.04
+	uv = cascade_control_all_nodes_rrtTree(graph_rrt_son,  graph_rrt_father, min_x=-1.5, min_y=-1.5, max_x=1.5, max_y=1.5, n_sample=301)  # TODO: change to rrt graph 08.04
 
 	vel_x = np.reshape(uv[:, 0], (n_sample, n_sample))
 	vel_y = np.reshape(uv[:, 1], (n_sample, n_sample))